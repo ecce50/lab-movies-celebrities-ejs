65142270ef9044f6972f9b88084631bf6ef957f5
Peter R <prodger2@gmail.com>
2023-07-10 21:21:17 +0200

    Done up to and including iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,17 +1,23 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require('express').Router()
 
+/* GET new Celebrity page */
+router.get('/new-celebrity', (req, res, next) => {
+  console.log(req)
+  res.render('celebrities/new-celebrity', { newCeleb })
+})
+
 // Create a new celebrity
-router.post('/new-celebrity', async (req, res, next) => {
+router.post('/celebrities/new-celebrity', async (req, res, next) => {
   // Get the data
   console.log(req.body)
   const data = req.body
   try {
     // Create a new document with the data
-    const newRecipe = await Recipe.create(data)
-    console.log(newRecipe)
+    const newCelebrity = await Celebrity.create(data)
+    console.log(newCelebrity)
     // Redirect to some page
-    res.redirect(`/recipes/${newRecipe._id}`)
+    res.redirect(`/celebrities/new-celebrity${newCelebrity._id}`)
   } catch (error) {
     console.log(error)
   }
